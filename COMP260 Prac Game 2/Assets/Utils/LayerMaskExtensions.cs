dec1cb93f5a1adc367b4336fbdfc1d5f7689fc99
Malcolm Ryan <malcolm.ryan@gmail.com>
2017-03-14 13:26:40 +1100

    Copied from old repository

---
+++ COMP260 Prac Game 2/Assets/Utils/LayerMaskExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class LayerMaskExtensions  {
+
+	/**
+	 * Check whether a particular gameObject belongs to one of the layers in this mask
+	 */
+
+	public static bool Contains(this LayerMask layerMask, GameObject gameObject) {
+		return (layerMask.value & (1 << gameObject.layer)) != 0;
+	}
+}
---
+++ COMP260 Prac Game 2/Assets/Utils/Vector3Extensions.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Some GLSL-like conversions from Vector3 to Vector2.
+ * Implemented as class extensions. So it should be possible to do:
+ * 
+ * Vector3 v3 = <blah>;
+ * Vector2 v2 = v3.zx();
+ */
+public static class Vector3Extensions  {
+
+	public static Vector2 xy(this Vector3 v) {
+		return v;
+	}
+
+	public static Vector2 xz(this Vector3 v) {
+		return new Vector2(v.x, v.z);
+	}
+
+	public static Vector2 yz(this Vector3 v) {
+		return new Vector2(v.y, v.z);
+	}
+
+	public static Vector2 yx(this Vector3 v) {
+		return new Vector2(v.y, v.x);
+	}
+
+	public static Vector2 zx(this Vector3 v) {
+		return new Vector2(v.z, v.x);
+	}
+
+	public static Vector2 zy(this Vector3 v) {
+		return new Vector2(v.z, v.y);
+	}
+
+	public static string longString(this Vector3 v) {
+		return string.Format("({0}, {1}, {2})", v.x, v.y, v.z);
+	}
+
+	public static Vector3 Rotate(this Vector3 v, float angle, Vector3 axis) {
+		return Quaternion.AngleAxis(angle, axis) * v;
+	}
+
+}
+
